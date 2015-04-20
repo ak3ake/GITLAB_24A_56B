8510f1f6e3cb0fffc092443532f2610bd19b1739
HYPERTASTIC <hypertastic42010@gmail.com>
2015-04-20 12:05:37 +0700

    modified program 2

--- Program2.c
+++ Program2.c
@@ -5,5 +5,7 @@ int main()
 	printf("Hello Kitty. That reminds me when I was Freshy \n");
 	printf("I'm HYPERTASTIC , Nice to meet you :) \n");
 	printf("My Display Profile is Dopamine from jubeat saucer fulfill , My favourite song!! ");
+	printf("Ake said F word to me \n");
+	printf("But I still love CPE333.............................or not?");
 	return(0);
 }
