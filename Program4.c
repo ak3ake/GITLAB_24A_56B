7c621e664a00568f8d26f2a85736dffc829c8d5a
HYPERTASTIC <hypertastic42010@gmail.com>
2015-04-20 12:30:07 +0700

    my very very sad story

---
+++ Program4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello Again \n");
+	printf("I love music games so much \n");
+	printf("But I don't have time to play them");
+	printf("too bad..... so sad");
+	return(0);
+}
