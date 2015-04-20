c10436f716ea1492111dde8bbecae5bdd9d48c29
ak3ake <P.Tungjitsirisun@gmail.com>
2015-04-20 12:11:45 +0700

    modified program1

--- program1.c
+++ program1.c
@@ -2,8 +2,15 @@
 
 int main()
 {
+	char array[10];	
+	int i=0;
+	
 	printf("Program#1\n");
 	printf("------------\n");
 	printf("Hello World!! From USER A: ak3ake\n");
 	printf("My ID: 55070501024\n");
+
+	printf("What is your name?\n");
+	for(i=0;i<10;i++)	
+		scanf("%c",array[i]);
 }
